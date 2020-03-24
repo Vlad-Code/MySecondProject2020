9c82079a26b3f9d28da6d844348207ed70712538
Vladislav Kungurov <vladislavkun27@gmail.com>
2020-03-24 21:22:22 +0400

    add options version, description and help to my cli app

---
+++ src/bin/index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const programm = require('commander');
+
+programm
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .parse(process.argv);
+
+if (!programm.args.length) programm.help();
