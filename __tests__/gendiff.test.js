e2f3ba14391ab74128a47d721b9b8f1e5498df9d
Vladislav Kungurov <vladislavkun27@gmail.com>
2020-04-03 13:17:05 +0400

    correct tests for github actions passing

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,8 +3,9 @@ import fs from 'fs';
 import gendiff from '../src/gendiff.js';
 
 test('gendiff', () => {
-  const firstConfig = 'before.json';
-  const secondConfig = 'after.json';
+  const firstConfig = `${__dirname}/../__fixtures__/before.json`;
+  console.log(firstConfig);
+  const secondConfig = `${__dirname}/../__fixtures__/after.json`;
   const result = fs.readFileSync(`${__dirname}/../__fixtures__/result.txt`, 'utf-8');
   expect(gendiff(firstConfig, secondConfig)).toEqual(result);
 });
